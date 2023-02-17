56017fc7884007bdbc0e864b0d1e8226d69d25c0
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-02-17 13:38:55 -0500

    Finish up UI and add buttons
    The remaining parts of the UI are added, along with two buttons, which
    have no function yet. One is meant to restart the grids, and the other
    is supposed to let the user pick the pixel size.

--- code.js
+++ code.js
@@ -1,15 +1,17 @@
 const cont = document.querySelector('.cont');
-for (let i = 0; i < 4; i++) {
-    let container = document.createElement('div');
-    container.classList.toggle('container');
-    cont.appendChild(container);
-    for (let i = 0; i < 4; i++) {
-        let sketch = document.createElement('div');
-        sketch.classList.toggle('sketch');
-        container.appendChild(sketch);
+function fillBox(pixels) {
+    for (let i = 0; i < pixels; i++) {
+        let container = document.createElement('div');
+        container.classList.toggle('container');
+        cont.appendChild(container);
+        for (let i = 0; i < pixels; i++) {
+            let sketch = document.createElement('div');
+            sketch.classList.toggle('sketch');
+            container.appendChild(sketch);
+        }
     }
 }
-
+fillBox(16)
 
 let sketch = document.querySelectorAll('.sketch');
 sketch.forEach(box => box.addEventListener('mouseover', () => box.classList.add('active')));
