886c664cdde34ed57ef761641f4e00c5023e20bc
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-02-16 20:37:43 -0500

    Create code to make 16x16 grid
    A for loop is created which makes 256 boxes inside a container, which
    is where the main etch-a-sketch takes part.

---
+++ code.js
@@ -0,0 +1,14 @@
+const cont = document.querySelector('.cont');
+for (let i = 0; i < 4; i++) {
+    let container = document.createElement('div');
+    container.classList.toggle('container');
+    cont.appendChild(container);
+    for (let i = 0; i < 4; i++) {
+        let sketch = document.createElement('div');
+        sketch.classList.toggle('sketch');
+        container.appendChild(sketch);
+    }
+}
+
+let sketch = document.querySelectorAll('.sketch');
+sketch.forEach(box => box.addEventListener('mouseover', () => box.classList.add('active')));
